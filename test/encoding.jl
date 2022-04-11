8d27cb2d3254920187d35c42345c0f06d1c42fc8
azzaare <jf@baffier.fr>
2022-04-11 17:02:44 +0900

    Fix encoding testa

--- test/encoding.jl
+++ test/encoding.jl
@@ -13,17 +13,12 @@ end
     x = [0,1,2,3,4,5]
 
     d = domain([0,1,2,3,4])
-    y = get_domain(d)
+    y = [0,1,2,3,4]
 
     a = [0,0,0,0,0, 1,0,0,0,0, 1,1,0,0,0, 1,1,1,0,0, 1,1,1,1,0, 1,1,1,1,1]
-    b = [1,0,0,0,0, 1,1,0,0,0, 1,1,1,0,0, 1,1,1,1,0, 1,1,1,1,1]
 
     @test collect(binarize(x; encoding = :domain_wall)) == a
     @test integerize(a; encoding = :domain_wall) == x
 
-    # @info collect(binarize(y, d; encoding = :domain_wall))
-    @info d integerize(b, d; encoding = :domain_wall)
-
-    # @test collect(binarize(y, domain([0,1,2,3,4]); encoding = :domain_wall)) == b
-    # @test integerize(b, domain([0,1,2,3,4]); encoding = :domain_wall) == y
+    @test integerize(collect(binarize(y, d; encoding = :domain_wall)), d; encoding = :domain_wall) == y
 end
