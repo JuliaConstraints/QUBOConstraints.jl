e92ce25c63e4852a64287ca440f95270330cef3e
Documenter.jl <documenter@juliadocs.github.io>
2022-04-14 09:14:09 +0000

    build based on 30abc6a

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QUBOConstraints","category":"page"},{"location":"#QUBOConstraints","page":"Home","title":"QUBOConstraints","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QUBOConstraints.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QUBOConstraints]","category":"page"}]
+}
